e239af60da07e2b9460280e9e17f1f9aac8d5fcc
chgrace <iamsywid@gmail.com>
2017-07-10 18:53:24 +0800

    Add class binding

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -8,18 +8,21 @@ import { Component } from '@angular/core';
     template: `
         <h2>Courses</h2>
         <img src="{{ imageUrl }}" />
+        <img [src]="imageUrl"/>
         <h2>{{ title }}</h2>
         <ul>
             <li *ngFor="let course of courses">
                 {{ course }}
             </li>
         </ul>
+        <button class="btn btn-primary" [class.active]="isActive">Save</button>
     `
 })
 export class CoursesComponent {
     title = "List of Courses";
     courses;
     imageUrl = "http://lorempixel.com/400/200";
+    isActive = false;
 
     constructor(service: CoursesService) {
         this.courses = service.getCourses();
